27afe3d938ad8a32fec2d71457b4b1fb465c7e20
s.palanque <s.palanque@genese.info>
2025-04-09 01:43:10 +0200

    push

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './components/Page';
-import PrivacyPolicy from './components/PrivacyPolicy';
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/Page";
+import PrivacyPolicy from "./components/PrivacyPolicy";
 
 function App() {
   return (
-    <Router>
+    <Router basename={window.location.pathname || ""}>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './index.css';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(<App />);
+
+root.render(
+  <React.StrictMode>
+    {/* Ajouter le basename ici pour gérer les routes dans GitHub Pages */}
+    <Router basename={window.location.pathname || ""}>
+      <App />
+    </Router>
+  </React.StrictMode>
+);
